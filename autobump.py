b13398c54fd7b2c68728d9db03cff18a611c6810
SippingSizzurp <111225194+SippingSizzurp@users.noreply.github.com>
2022-08-15 03:33:30 -0400

    Add files via upload

---
+++ autobump.py
@@ -0,0 +1,22 @@
+from requests import request
+from random import randint
+from time import sleep
+from discordwebhook import Discord
+
+urls = []
+webhooks = []
+
+
+while True:
+    for i in webhooks:
+        BUMP_WEBHOOK = Discord(url=f'{webhooks}')
+        BUMP_WEBHOOK.post(content='AUTOBUMP HAS STARTED', username='DISBOARD')
+
+    TIME_TO_WAIT = randint(7200, 9000)
+    TIME_TO_WAIT_MINUTES = TIME_TO_WAIT / 60
+    TIME_TO_WAIT_MINUTES_ROUNDED = int(TIME_TO_WAIT_MINUTES) / 1
+
+    for i in urls:
+        DISBOARD_REQUEST = request(url=f'', method='get')
+        print(DISBOARD_REQUEST.status_code)
+    sleep(TIME_TO_WAIT_MINUTES_ROUNDED)
