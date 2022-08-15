f392e18d855cfbd5540f4fcfcda256c05347001e
SippingSizzurp <111225194+SippingSizzurp@users.noreply.github.com>
2022-08-15 03:34:06 -0400

    Update autobump.py

--- autobump.py
+++ autobump.py
@@ -6,17 +6,20 @@ from discordwebhook import Discord
 urls = []
 webhooks = []
 
+for i in webhooks:
+    BUMP_WEBHOOK = Discord(url=f'{i}')
+    BUMP_WEBHOOK.post(content='AUTOBUMP HAS STARTED', username='DISBOARD')
 
 while True:
     for i in webhooks:
-        BUMP_WEBHOOK = Discord(url=f'{webhooks}')
-        BUMP_WEBHOOK.post(content='AUTOBUMP HAS STARTED', username='DISBOARD')
+        BUMP_WEBHOOK = Discord(url=f'{i}')
+        BUMP_WEBHOOK.post(content='SERVER HAS BEEN AUTOBUMPED!', username='DISBOARD')
 
     TIME_TO_WAIT = randint(7200, 9000)
     TIME_TO_WAIT_MINUTES = TIME_TO_WAIT / 60
     TIME_TO_WAIT_MINUTES_ROUNDED = int(TIME_TO_WAIT_MINUTES) / 1
 
     for i in urls:
-        DISBOARD_REQUEST = request(url=f'', method='get')
+        DISBOARD_REQUEST = request(url=f'{i}', method='get')
         print(DISBOARD_REQUEST.status_code)
     sleep(TIME_TO_WAIT_MINUTES_ROUNDED)
